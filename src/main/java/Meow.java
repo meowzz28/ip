c5f476bdfb72e2209409e10f9470e3e955535230
roger <rogerlim799@gmail.com>
2025-08-16 02:41:02 +0800

    Rename Duke to Meow and implemented skeletal version of Meow

---
+++ src/main/java/Meow.java
@@ -0,0 +1,6 @@
+public class Meow {
+    public static void main(String[] args) {
+        String logo = "Hello! I'm Meow\nWhat can I do for you?\nBye. Hope to see you again soon!";
+        System.out.println(logo);
+    }
+}
