af3c92602bc0de64c1472a9cfc3af841bbb35f6b
roger <rogerlim799@gmail.com>
2025-08-16 20:14:24 +0800

    Completed Level-5: Handle Errors

--- src/main/java/Meow.java
+++ src/main/java/Meow.java
@@ -10,35 +10,80 @@ public class Meow {
         printGreetMessage();
 
         while (true) {
+
             String input = scanner.nextLine();
             String[] words = input.split(" ");
-            if (input.equals("bye")) {
-                printExitMessage();
-                break;
-            } else if (input.equals("list")) {
-                printList();
-            } else if (words[0].equals("mark")) {
-                markDone(Integer.parseInt(words[1]) - 1);
-            } else if (words[0].equals("unmark")) {
-                markUndone(Integer.parseInt(words[1]) - 1);
-            } else if (words[0].equals("todo")) {
-                String description = input.substring("todo".length()).trim();
-                Todo t = new Todo(description);
-                printAddedTask(t);
-            } else if (words[0].equals("deadline")) {
-                String[] parts = input.split(" /by ", 2);
-                String description = parts[0].substring("deadline".length()).trim();
-                String by = parts[1].trim();
-                Deadline t = new Deadline(description, by);
-                printAddedTask(t);
-            } else if (words[0].equals("event")) {
-                String[] firstSplit = input.split(" /from ", 2);
-                String description = firstSplit[0].substring("event".length()).trim();
-                String[] secondSplit = firstSplit[1].split(" /to ", 2);
-                String from = secondSplit[0].trim();
-                String to = secondSplit[1].trim();
-                Event t = new Event(description, from, to);
-                printAddedTask(t);
+
+            try {
+                if (input.equals("bye")) {
+
+                    printExitMessage();
+                    break;
+
+                } else if (input.equals("list")) {
+
+                    printList();
+
+                } else if (words[0].equals("mark")) {
+
+                    if (words.length < 2) {
+                        throw new MeowException("OOPS!!! Please tell me which task number to mark.");
+                    }
+                    markDone(Integer.parseInt(words[1]) - 1);
+
+                } else if (words[0].equals("unmark")) {
+
+                    if (words.length < 2) {
+                        throw new MeowException("OOPS!!! Please tell me which task number to unmark.");
+                    }
+                    markUndone(Integer.parseInt(words[1]) - 1);
+
+                } else if (words[0].equals("todo")) {
+
+                    String description = input.substring("todo".length()).trim();
+                    if (description.isEmpty()) {
+                        throw new MeowException("OOPS!!! The description of a todo cannot be empty.");
+                    }
+                    Todo t = new Todo(description);
+                    printAddedTask(t);
+
+                } else if (words[0].equals("deadline")) {
+
+                    if (!input.contains("/by")) {
+                        throw new MeowException("OOPS!!! A deadline must include '/by'. Example: deadline return book /by Sunday");
+                    }
+                    String[] parts = input.split(" /by ", 2);
+                    String description = parts[0].substring("deadline".length()).trim();
+                    if (description.isEmpty()) {
+                        throw new MeowException("OOPS!!! The description of a deadline cannot be empty!");
+                    }
+                    String by = parts[1].trim();
+                    Deadline t = new Deadline(description, by);
+                    printAddedTask(t);
+
+                } else if (words[0].equals("event")) {
+
+                    if (!input.contains("/from") || !input.contains("/to")) {
+                        throw new MeowException("OOPS!!! An event must include both '/from' and '/to'. Example: event meeting /from Mon 2pm /to 4pm");
+                    }
+                    String[] firstSplit = input.split(" /from ", 2);
+                    String description = firstSplit[0].substring("event".length()).trim();
+                    if (description.isEmpty()) {
+                        throw new MeowException("OOPS!!! The description of an event cannot be empty!");
+                    }
+                    String[] secondSplit = firstSplit[1].split(" /to ", 2);
+                    String from = secondSplit[0].trim();
+                    String to = secondSplit[1].trim();
+                    Event t = new Event(description, from, to);
+                    printAddedTask(t);
+
+                } else {
+                    throw new MeowException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (MeowException e) {
+                printLine();
+                System.out.println("\t" + e.getMessage());
+                printLine();
             }
         }
     }
