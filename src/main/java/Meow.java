e679302aa16ab5617d8fea4fbd5cb943dd79c4ae
roger <rogerlim799@gmail.com>
2025-08-16 13:11:35 +0800

    Completed Level-1: Improved the skeletal version of Meow

--- src/main/java/Meow.java
+++ src/main/java/Meow.java
@@ -1,6 +1,41 @@
+import java.util.Scanner;
+
 public class Meow {
+    private Scanner scanner = new Scanner(System.in);
+
+    private void startRun() {
+        printGreetMessage();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                printExitMessage();
+                break;
+            } else {
+                printMessage(input);
+            }
+        }
+    }
+
+    private void printGreetMessage() {
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Meow\n\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+    }
+
+    private void printExitMessage() {
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t Bye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
+    }
+
+    private void printMessage(String input) {
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t" + input);
+        System.out.println("\t____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String logo = "Hello! I'm Meow\nWhat can I do for you?\nBye. Hope to see you again soon!";
-        System.out.println(logo);
+        Meow meow = new Meow();
+        meow.startRun();
     }
 }
