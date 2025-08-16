c283609208dc70185e4442cdb0560672a8f6966e
roger <rogerlim799@gmail.com>
2025-08-16 18:02:44 +0800

    Completed Level-3: Added mark/unmark as done feature

--- src/main/java/Meow.java
+++ src/main/java/Meow.java
@@ -2,48 +2,78 @@ import java.util.Scanner;
 
 public class Meow {
     private final Scanner scanner = new Scanner(System.in);
-    private final String[] list = new String[100];
+    private final Task[] list = new Task[100];
     private int count = 0;
 
     private void startRun() {
+
         printGreetMessage();
+
         while (true) {
             String input = scanner.nextLine();
+            String[] words = input.split(" ");
             if (input.equals("bye")) {
                 printExitMessage();
                 break;
-            } else if(input.equals("list")) {
+            } else if (input.equals("list")) {
                 printList();
-            } else {
+            } else if (words[0].equals("mark")) {
+                markDone(Integer.parseInt(words[1]) - 1);
+            } else if (words[0].equals("unmark")) {
+                markUndone(Integer.parseInt(words[1]) - 1);
+            }else {
                 printMessage(input);
             }
         }
     }
 
     private void printGreetMessage() {
-        System.out.println("\t____________________________________________________________");
+        printLine();
         System.out.println("\tHello! I'm Meow\n\tWhat can I do for you?");
-        System.out.println("\t____________________________________________________________");
+        printLine();
     }
 
     private void printExitMessage() {
-        System.out.println("\t____________________________________________________________");
+        printLine();
         System.out.println("\t Bye. Hope to see you again soon!");
-        System.out.println("\t____________________________________________________________");
+        printLine();
     }
 
     private void printMessage(String input) {
-        System.out.println("\t____________________________________________________________");
+        Task newTask = new Task(input);
+        list[count++] = newTask;
+        printLine();
         System.out.println("\tadded: " + input);
-        System.out.println("\t____________________________________________________________");
-        list[count++] = input;
+        printLine();
     }
 
     private void printList() {
-        System.out.println("\t____________________________________________________________");
+        printLine();
         for(int i = 0; i < count; i++){
             System.out.println("\t" + (i + 1) + ". " + list[i]);
         }
+        printLine();
+    }
+
+    private void markDone(int index) {
+        Task task = list[index];
+        task.markDone();
+        printLine();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  " + task);
+        printLine();
+    }
+
+    private void markUndone(int index) {
+        Task task = list[index];
+        task.markUndone();
+        printLine();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("  " + task);
+        printLine();
+    }
+
+    private void printLine() {
         System.out.println("\t____________________________________________________________");
     }
 
