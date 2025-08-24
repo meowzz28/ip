328c8a7394c8e75666e6032071d7cfceb384cc8e
roger <rogerlim799@gmail.com>
2025-08-24 13:44:00 +0800

    Add save tasks feature

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -12,4 +12,9 @@ public class Event extends Task{
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + " )";
     }
+
+    @Override
+    public String saveTaskString() {
+        return "E | " + (this.isDone ? "1" : "0") + " | " + this.description + " | " + this.from + " | " + this.to;
+    }
 }
