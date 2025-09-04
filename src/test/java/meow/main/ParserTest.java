d30b6edf8b1c6214564132fd8ba324e39dabde91
roger <rogerlim799@gmail.com>
2025-09-04 17:41:10 +0800

    Add GUI to Meow

--- src/test/java/meow/main/ParserTest.java
+++ src/test/java/meow/main/ParserTest.java
@@ -1,7 +1,6 @@
 package meow.main;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -12,56 +11,30 @@ import meow.task.Task;
 
 public class ParserTest {
     private TaskList tasks;
-    private StubUi ui;
+    private Ui ui;
     private StubStorage storage;
 
     @BeforeEach
     public void setup() {
         tasks = new TaskList();
-        ui = new StubUi();
+        ui = new Ui();
         storage = new StubStorage();
     }
 
     @Test
     public void addCase_todo_addsTodoSuccess() throws MeowException {
-        boolean exit = Parser.parse("todo read book", tasks, ui, storage);
-        assertFalse(exit);
+        String response = Parser.parse("todo read book", tasks, ui, storage);
         assertEquals(1, tasks.size());
         Task t = tasks.get(0);
         assertEquals("[T][ ] read book", t.toString());
-        assertTrue(ui.lastMessage.contains("added"));
+        assertTrue(response.contains("added"));
         assertTrue(storage.saved, "Storage should be saved");
     }
 
     @Test
     public void exitCase_bye_exitSuccess() throws MeowException {
-        boolean exit = Parser.parse("bye", tasks, ui, storage);
-        assertTrue(exit);
-        assertTrue(ui.lastMessage.contains("Bye"));
-    }
-
-    private static class StubUi extends Ui {
-        private String lastMessage = "";
-        @Override
-        public void printExitMessage() {
-            lastMessage = "Bye!";
-        }
-        @Override
-        public void printAddedTask(Task t, int size) {
-            lastMessage = "added: " + t.toString();
-        }
-        @Override
-        public void printTasks(TaskList tasks) {
-            lastMessage = "listing tasks";
-        }
-        @Override
-        public void printDeletedTask(Task t, int size) {
-            lastMessage = "deleted: " + t.toString();
-        }
-        @Override
-        public void printMarked(Task t) {
-            lastMessage = "marked: " + t.toString();
-        }
+        String response = Parser.parse("bye", tasks, ui, storage);
+        assertTrue(response.contains("Bye"), "Response should contain 'Bye'");
     }
 
     private static class StubStorage extends Storage {
